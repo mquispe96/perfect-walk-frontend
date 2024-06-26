b6f0edecc97fe2b5b615d0a4af058829b2948472
mquispe96 <mquispe@pursuit.org>
2024-06-26 00:22:41 -0400

    Organized file structure, created main jsx files for pages, and setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import {Route, Routes} from 'react-router-dom';
+import Layout from './Pages/Layout-Content/Layout';
+import Home from './Pages/Home-Content/Home';
+import Weather from './Pages/Weather-Content/Weather';
+import Places from './Pages/Places-Content/Places';
+import About from './Pages/About-Content/About';
 
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="Weather" element={<Weather />} />
+        <Route path="Places" element={<Places />} />
+        <Route path="About" element={<About />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
