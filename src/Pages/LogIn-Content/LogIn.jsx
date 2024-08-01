3cb2d3bb65f42b579cfa40a4b79117c46de0afdb
mquispe96 <mquispe@pursuit.org>
2024-08-01 00:49:31 -0400

    Styled login page

--- src/Pages/LogIn-Content/LogIn.jsx
+++ src/Pages/LogIn-Content/LogIn.jsx
@@ -1,12 +1,12 @@
 import { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { PageContext } from "../Context/PageContext";
 import { LuEye } from "react-icons/lu";
 import { LuEyeOff } from "react-icons/lu";
 import { HiUserCircle } from "react-icons/hi";
 import { TbPasswordUser } from "react-icons/tb";
-import './Styling/login.css';
+import "./Styling/login.css";
 
 const LogIn = () => {
   const BASE_URL = import.meta.env.VITE_API_URL;
@@ -36,62 +36,80 @@ const LogIn = () => {
       })
       .catch((err) => {
         setError(err.response.data.error);
+        setTimeout(() => {
+          setError("");
+        }, 3000);
       });
   };
 
   return (
-    <form className="login-form" onSubmit={handleSubmit}>
-      <div className="login-form__section">
-        <h2>Log In</h2>
-      </div>
-      <div className="login-form__section">
-        <div className="username">
-          <HiUserCircle className="icon" />
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={credentials.username}
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
+    <main className="form-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="login-form__section">
+          <h2>Log In</h2>
         </div>
-      </div>
-      <div className="login-form__section">
-        <div className="password">
-          <TbPasswordUser className="icon" />
-          <input
-            type={showPassword ? "text" : "password"}
-            name="password"
-            id="password"
-            value={credentials.password}
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-          {showPassword ? (
-            <LuEyeOff
-              onClick={() => setShowPassword(false)}
+        <div className="login-form__section">
+          <div className="username">
+            <HiUserCircle />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={credentials.username}
+              onChange={handleChange}
+              required
+              autoComplete="off"
+              placeholder="Username"
             />
-          ) : (
-            <LuEye onClick={() => setShowPassword(true)} />
-          )}
+          </div>
         </div>
-      </div>
-      <div className="login-form__section main-btns">
-        <button type="button" onClick={() => navigate(-1)}>Cancel</button>
-        <button type="submit">Log In</button>
-      </div>
-      <div className="login-form__section switch-btn">
-        <button type="button" onClick={() => navigate("/signup")}>
-          Sign Up
-        </button>
-      </div>
-      <div className="login-form__section error">
-        {error && <p className="error">{error}</p>}
-      </div>
-    </form>
+        <div className="login-form__section">
+          <div className="password">
+            <TbPasswordUser />
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              id="password"
+              value={credentials.password}
+              onChange={handleChange}
+              required
+              autoComplete="off"
+              placeholder="Password"
+            />
+            {showPassword ? (
+              <LuEyeOff
+                style={{ cursor: "pointer" }}
+                onClick={() => setShowPassword(false)}
+              />
+            ) : (
+              <LuEye
+                style={{ cursor: "pointer" }}
+                onClick={() => setShowPassword(true)}
+              />
+            )}
+          </div>
+        </div>
+        <div className="login-form__section main-btns">
+          <button type="button" onClick={() => navigate(-1)}>
+            Cancel
+          </button>
+          <button type="submit">Log In</button>
+        </div>
+        <div className="login-form__section">
+          <Link className="forgot-link" to="/forgot-password">
+            Forgot Password?
+          </Link>
+        </div>
+        <div className="login-form__section switch-btn">
+          <button type="button" onClick={() => navigate("/signup")}>
+            Sign Up
+          </button>
+        </div>
+        <div className="login-form__section error">
+          {error && <p className="error">{error}</p>}
+        </div>
+      </form>
+    </main>
   );
 };
 
